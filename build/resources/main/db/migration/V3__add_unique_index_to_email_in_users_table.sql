c11db3f941a10707df7a6c5dd072a61e8057bd6b
Liam Kokab <Liam@Kokab.no>
2025-04-14 15:35:43 +0200

    Delete build directory

--- build/resources/main/db/migration/V3__add_unique_index_to_email_in_users_table.sql
+++
@@ -1,6 +0,0 @@
--- Add a unique constraint and index to the "email" column in the "users" table
-ALTER TABLE users
-    ADD CONSTRAINT unique_email UNIQUE (email);
-
--- Create an index on the "email" column
-CREATE INDEX idx_email ON users (email);
